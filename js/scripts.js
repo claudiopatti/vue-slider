31ce445e946be08c9673a40664f09557cc4f5b8d
claudiopatti <claudiopatti1997@gmail.com>
2024-08-29 23:01:15 +0200

    aggiunto carosello su imagini e pure sul Thumb

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,61 @@ const { createApp } = Vue
     createApp({
         data() {
         return {
-            message: 'Hello Vue!'
+            i : 0,
+
+            slides : [
+                    {
+                        image: 'img/01.webp',
+                        title: 'Marvel\'s Spiderman Miles Morale',
+                        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                        }, 
+                    {
+                        image: 'img/02.webp',
+                        title: 'Ratchet & Clank: Rift Apart',
+                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    }, {
+                        image: 'img/03.webp',
+                        title: 'Fortnite',
+                        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    }, {
+                        image: 'img/04.webp',
+                        title: 'Stray',
+                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    }, {
+                        image: 'img/05.webp',
+                        title: "Marvel's Avengers",
+                        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    }
+                    ],
+            i : 0,
+
+        };
+    },
+    methods: {
+
+        nextImg () {
+            if(this.i<4) {
+                this.i++;
+            }
+            else {
+                this.i = 0;
+            }
+            console.log('i', this.i);
+            
+        },
+        prevImg () {
+            if(this.i != 0) {
+                this.i--;
+            }
+            else {
+                this.i = 4;
+            }
+        },
+
+        changeActive (slidesIndex) {
+            this.i = slidesIndex
         }
-        }
+        
+        
+    }
     }).mount('#app')
